88bbb074c81b775bb2e8d61aade07003c2ebc0aa
fwar2002 <123962321+fwar2002@users.noreply.github.com>
2023-03-30 17:01:36 -0500

    Update home_page.dart

--- lib/src/screens/home_page.dart
+++ lib/src/screens/home_page.dart
@@ -131,7 +131,6 @@ class _HomePageState extends State<HomePage>{
               ],
             ),
           ),
-
           SizedBox(
             width: double.infinity,
             child: Column(
