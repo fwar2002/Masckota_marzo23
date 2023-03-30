6e225e549bd228856f12df7cbb62ebfb6a6db2a7
fwar2002 <123962321+fwar2002@users.noreply.github.com>
2023-03-30 17:10:12 -0500

    commit
    commit

--- lib/src/components/categories_vert_listview_.dart
+++ lib/src/components/categories_vert_listview_.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class CategoriesList extends StatelessWidget{
+class CategoriesVerticalList extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Container(
-      height: 125.0,
+      height: 70.0,
       child: ListView(
         scrollDirection: Axis.horizontal,
         children:  <Widget>[
           SizedBox(
-            height: 130,
+            height: 70,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -30,145 +30,28 @@ class CategoriesList extends StatelessWidget{
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: SizedBox(
-                          height: 102,
-                          width: 95,
+                          height: 50,
+                          width: 185,
                           //decoration: const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
-                          child: Column(
+                          child: Row(
                             children: const <Widget>[
                               CircleAvatar(
-                                maxRadius: 40,
-                                backgroundColor: Colors.deepOrange,
-                                child: Image(
-                                  color: Colors.white,
-                                  image: AssetImage('lib/src/assets/images/categories/main_categories_dog.png'),
-                                  width: 47,
-                                ),
-                              ),
-                              const SizedBox(height: 3),
-                              Text(
-                                'Perros',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 13,
-                                ),
-                              ),                          //const SizedBox(height: 2),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Card(
-                      elevation: 0,
-                      shadowColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        side: const BorderSide(
-                          color: Colors.grey,
-                        ),
-                        borderRadius: BorderRadius.circular(7.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 102,
-                          width: 95,
-                          //decoration: const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
-                          child: Column(
-                            children: const <Widget>[
-                              CircleAvatar(
-                                maxRadius: 40,
-                                backgroundColor: Colors.pinkAccent,
-                                child: Image(
-                                  color: Colors.white,
-                                  image: AssetImage('lib/src/assets/images/categories/main_categories_cat.png'),
-                                  width: 47,
-                                ),
-                              ),
-                              const SizedBox(height: 3),
-                              Text(
-                                'Gatos',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 13,
-                                ),
-                              ),                          //const SizedBox(height: 2),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Card(
-                      elevation: 0,
-                      shadowColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        side: const BorderSide(
-                          color: Colors.grey,
-                        ),
-                        borderRadius: BorderRadius.circular(7.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 102,
-                          width: 95,
-                          //decoration: const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
-                          child: Column(
-                            children: const <Widget>[
-                              CircleAvatar(
-                                maxRadius: 40,
-                                backgroundColor: Colors.indigoAccent,
-                                child: Image(
-                                  color: Colors.white,
-                                  image: AssetImage('lib/src/assets/images/categories/main_categories_hamster.png'),
-                                  width: 47,
-                                ),
-                              ),
-                              const SizedBox(height: 3),
-                              Text(
-                                'Hamsters',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 13,
-                                ),
-                              ),                          //const SizedBox(height: 2),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Card(
-                      elevation: 0,
-                      shadowColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        side: const BorderSide(
-                          color: Colors.grey,
-                        ),
-                        borderRadius: BorderRadius.circular(7.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 102,
-                          width: 95,
-                          //decoration: const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
-                          child: Column(
-                            children: const <Widget>[
-                              CircleAvatar(
-                                maxRadius: 40,
+                                maxRadius: 25,
                                 backgroundColor: Colors.red,
                                 child: Image(
                                   color: Colors.white,
-                                  image: AssetImage('lib/src/assets/images/categories/main_categories_bird.png'),
-                                  width: 47,
+                                  image: AssetImage('lib/src/assets/images/categories/main_categories_clinic.png'),
+                                  width: 35,
                                 ),
                               ),
-                              const SizedBox(height: 3),
+                              SizedBox(width: 8),
                               Text(
-                                'Pájaros',
+                                'Clínicas Veterinarias',
                                 style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 13,
                                 ),
-                              ),                          //const SizedBox(height: 2),
+                              ),
                             ],
                           ),
                         ),
@@ -183,31 +66,32 @@ class CategoriesList extends StatelessWidget{
                         ),
                         borderRadius: BorderRadius.circular(7.0),
                       ),
+                      //color: Colors.deepOrangeAccent[100],
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 102,
-                          width: 95,
+                        child: SizedBox(
+                          height: 50,
+                          width: 185,
                           //decoration: const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
-                          child: Column(
+                          child: Row(
                             children: const <Widget>[
                               CircleAvatar(
-                                maxRadius: 40,
-                                backgroundColor: Colors.teal,
+                                maxRadius: 25,
+                                backgroundColor: Colors.blue,
                                 child: Image(
                                   color: Colors.white,
-                                  image: AssetImage('lib/src/assets/images/categories/main_categories_fish.png'),
-                                  width: 47,
+                                  image: AssetImage('lib/src/assets/images/categories/main_categories_dog.png'),
+                                  width: 35,
                                 ),
                               ),
-                              const SizedBox(height: 3),
+                              SizedBox(width: 8),
                               Text(
-                                'Peces',
+                                'Peluquerías caninas',
                                 style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 13,
                                 ),
-                              ),                          //const SizedBox(height: 2),
+                              ),
                             ],
                           ),
                         ),
@@ -222,31 +106,152 @@ class CategoriesList extends StatelessWidget{
                         ),
                         borderRadius: BorderRadius.circular(7.0),
                       ),
+                      //color: Colors.deepOrangeAccent[100],
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 102,
-                          width: 95,
+                        child: SizedBox(
+                          height: 50,
+                          width: 185,
                           //decoration: const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
-                          child: Column(
+                          child: Row(
                             children: const <Widget>[
                               CircleAvatar(
-                                maxRadius: 40,
+                                maxRadius: 25,
                                 backgroundColor: Colors.purple,
                                 child: Image(
                                   color: Colors.white,
-                                  image: AssetImage('lib/src/assets/images/categories/main_categories_rabbit.png'),
-                                  width: 47,
+                                  image: AssetImage('lib/src/assets/images/categories/main_categories_shop.png'),
+                                  width: 35,
                                 ),
                               ),
-                              const SizedBox(height: 3),
+                              SizedBox(width: 8),
                               Text(
-                                'Conejos',
+                                'Tiendas',
                                 style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 13,
                                 ),
-                              ),                          //const SizedBox(height: 2),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 0,
+                      shadowColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(7.0),
+                      ),
+                      //color: Colors.deepOrangeAccent[100],
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          height: 50,
+                          width: 185,
+                          //decoration: const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+                          child: Row(
+                            children: const <Widget>[
+                              CircleAvatar(
+                                maxRadius: 25,
+                                backgroundColor: Colors.blueGrey,
+                                child: Image(
+                                  color: Colors.white,
+                                  image: AssetImage('lib/src/assets/images/categories/main_categories_lab.png'),
+                                  width: 35,
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                'Laboratorios',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 0,
+                      shadowColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(7.0),
+                      ),
+                      //color: Colors.deepOrangeAccent[100],
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          height: 50,
+                          width: 185,
+                          //decoration: const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+                          child: Row(
+                            children: const <Widget>[
+                              CircleAvatar(
+                                maxRadius: 25,
+                                backgroundColor: Colors.teal,
+                                child: Image(
+                                  color: Colors.white,
+                                  image: AssetImage('lib/src/assets/images/categories/main_categories_hotel.png'),
+                                  width: 35,
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                'Hotel canino',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 0,
+                      shadowColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(7.0),
+                      ),
+                      //color: Colors.deepOrangeAccent[100],
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          height: 50,
+                          width: 185,
+                          //decoration: const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
+                          child: Row(
+                            children: const <Widget>[
+                              CircleAvatar(
+                                maxRadius: 25,
+                                backgroundColor: Colors.redAccent,
+                                child: Image(
+                                  color: Colors.white,
+                                  image: AssetImage('lib/src/assets/images/categories/main_categories_form.png'),
+                                  width: 35,
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                'Jornadas',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13,
+                                ),
+                              ),
                             ],
                           ),
                         ),
